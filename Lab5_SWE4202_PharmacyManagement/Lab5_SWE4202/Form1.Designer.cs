a8919c99c7926dbe272b0f15ba85f210d5f6b72d
Nazia-Karim-Khan-Oishee <95170855+Nazia-Karim-Khan-Oishee@users.noreply.github.com>
2021-12-16 10:30:02 +0600

    Add files via upload

---
+++ Lab5_SWE4202_PharmacyManagement/Lab5_SWE4202/Form1.Designer.cs
@@ -0,0 +1,281 @@
+﻿namespace Lab5_SWE4202
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.MedicineName = new System.Windows.Forms.TextBox();
+            this.Inquantity = new System.Windows.Forms.TextBox();
+            this.Price = new System.Windows.Forms.TextBox();
+            this.MedID = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.Add = new System.Windows.Forms.Button();
+            this.Sell = new System.Windows.Forms.Button();
+            this.label7 = new System.Windows.Forms.Label();
+            this.MedicineID = new System.Windows.Forms.TextBox();
+            this.soldq = new System.Windows.Forms.TextBox();
+            this.CheckID = new System.Windows.Forms.TextBox();
+            this.CurrQ = new System.Windows.Forms.Label();
+            this.Show = new System.Windows.Forms.Button();
+            this.ShowBalance = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(23, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(47, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Name:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(23, 131);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(58, 16);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Quantity:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(23, 95);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(41, 16);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Price:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(23, 57);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(23, 16);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "ID:";
+            // 
+            // MedicineName
+            // 
+            this.MedicineName.Location = new System.Drawing.Point(99, 25);
+            this.MedicineName.Name = "MedicineName";
+            this.MedicineName.Size = new System.Drawing.Size(100, 22);
+            this.MedicineName.TabIndex = 4;
+            // 
+            // Inquantity
+            // 
+            this.Inquantity.Location = new System.Drawing.Point(99, 131);
+            this.Inquantity.Name = "Inquantity";
+            this.Inquantity.Size = new System.Drawing.Size(100, 22);
+            this.Inquantity.TabIndex = 5;
+            // 
+            // Price
+            // 
+            this.Price.Location = new System.Drawing.Point(99, 95);
+            this.Price.Name = "Price";
+            this.Price.Size = new System.Drawing.Size(100, 22);
+            this.Price.TabIndex = 6;
+            // 
+            // MedID
+            // 
+            this.MedID.Location = new System.Drawing.Point(99, 57);
+            this.MedID.Name = "MedID";
+            this.MedID.Size = new System.Drawing.Size(100, 22);
+            this.MedID.TabIndex = 7;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(381, 31);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(158, 16);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Medicine Identification ID:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(390, 63);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(58, 16);
+            this.label6.TabIndex = 9;
+            this.label6.Text = "Quantity:";
+            // 
+            // Add
+            // 
+            this.Add.Location = new System.Drawing.Point(89, 180);
+            this.Add.Name = "Add";
+            this.Add.Size = new System.Drawing.Size(75, 23);
+            this.Add.TabIndex = 10;
+            this.Add.Text = "Add";
+            this.Add.UseVisualStyleBackColor = true;
+            this.Add.Click += new System.EventHandler(this.Add_Click);
+            // 
+            // Sell
+            // 
+            this.Sell.Location = new System.Drawing.Point(477, 107);
+            this.Sell.Name = "Sell";
+            this.Sell.Size = new System.Drawing.Size(75, 23);
+            this.Sell.TabIndex = 11;
+            this.Sell.Text = "Sell";
+            this.Sell.UseVisualStyleBackColor = true;
+            this.Sell.Click += new System.EventHandler(this.Sell_Click);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(12, 268);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(158, 16);
+            this.label7.TabIndex = 12;
+            this.label7.Text = "Medicine Identification ID:";
+            // 
+            // MedicineID
+            // 
+            this.MedicineID.Location = new System.Drawing.Point(580, 31);
+            this.MedicineID.Name = "MedicineID";
+            this.MedicineID.Size = new System.Drawing.Size(100, 22);
+            this.MedicineID.TabIndex = 13;
+            // 
+            // soldq
+            // 
+            this.soldq.Location = new System.Drawing.Point(580, 63);
+            this.soldq.Name = "soldq";
+            this.soldq.Size = new System.Drawing.Size(100, 22);
+            this.soldq.TabIndex = 14;
+            // 
+            // CheckID
+            // 
+            this.CheckID.Location = new System.Drawing.Point(197, 268);
+            this.CheckID.Name = "CheckID";
+            this.CheckID.Size = new System.Drawing.Size(100, 22);
+            this.CheckID.TabIndex = 15;
+            // 
+            // CurrQ
+            // 
+            this.CurrQ.AutoSize = true;
+            this.CurrQ.Location = new System.Drawing.Point(12, 302);
+            this.CurrQ.Name = "CurrQ";
+            this.CurrQ.Size = new System.Drawing.Size(103, 16);
+            this.CurrQ.TabIndex = 16;
+            this.CurrQ.Text = "Current Quantity:";
+            // 
+            // Show
+            // 
+            this.Show.Location = new System.Drawing.Point(89, 336);
+            this.Show.Name = "Show";
+            this.Show.Size = new System.Drawing.Size(75, 23);
+            this.Show.TabIndex = 18;
+            this.Show.Text = "Show";
+            this.Show.UseVisualStyleBackColor = true;
+            this.Show.Click += new System.EventHandler(this.Show_Click);
+            // 
+            // ShowBalance
+            // 
+            this.ShowBalance.Location = new System.Drawing.Point(455, 245);
+            this.ShowBalance.Name = "ShowBalance";
+            this.ShowBalance.Size = new System.Drawing.Size(146, 23);
+            this.ShowBalance.TabIndex = 19;
+            this.ShowBalance.Text = "Show Balance";
+            this.ShowBalance.UseVisualStyleBackColor = true;
+            this.ShowBalance.Click += new System.EventHandler(this.ShowBalance_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.GrayText;
+            this.pictureBox1.Location = new System.Drawing.Point(339, 25);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(10, 413);
+            this.pictureBox1.TabIndex = 21;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.ShowBalance);
+            this.Controls.Add(this.Show);
+            this.Controls.Add(this.CurrQ);
+            this.Controls.Add(this.CheckID);
+            this.Controls.Add(this.soldq);
+            this.Controls.Add(this.MedicineID);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.Sell);
+            this.Controls.Add(this.Add);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.MedID);
+            this.Controls.Add(this.Price);
+            this.Controls.Add(this.Inquantity);
+            this.Controls.Add(this.MedicineName);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "PharmacyManagement";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox MedicineName;
+        private System.Windows.Forms.TextBox Inquantity;
+        private System.Windows.Forms.TextBox Price;
+        private System.Windows.Forms.TextBox MedID;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button Add;
+        private System.Windows.Forms.Button Sell;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox MedicineID;
+        private System.Windows.Forms.TextBox soldq;
+        private System.Windows.Forms.TextBox CheckID;
+        private System.Windows.Forms.Label CurrQ;
+        private System.Windows.Forms.Button Show;
+        private System.Windows.Forms.Button ShowBalance;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ Lab5_SWE4202_PharmacyManagement/Lab5_SWE4202/Form1.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using PharmacyManagement;
+namespace Lab5_SWE4202
+{
+    public partial class Form1 : Form
+    {
+        public List<Medicine> listofmed = new List<Medicine>();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Add_Click(object sender, EventArgs e)
+        {
+            int flag = 0;
+            foreach (Medicine medicine in listofmed)
+            {
+                if (medicine.getid() == MedID.Text)
+                {
+                    medicine.setinquantity(Convert.ToInt32(Inquantity.Text));
+                    flag = 1;
+                    MessageBox.Show("Medicine quantity has been updated.");
+                    break;
+                }
+            }
+            if (flag == 0)
+            {
+                Medicine medicine1 = new Medicine(MedicineName.Text, MedID.Text, Convert.ToInt32(Price.Text), Convert.ToInt32(Inquantity.Text));
+                listofmed.Add(medicine1);
+                MessageBox.Show("This medicine has been added.");
+            }
+                
+        }
+
+        private void Sell_Click(object sender, EventArgs e)
+        {
+            string sellid = MedicineID.Text;
+            foreach (Medicine medicine in listofmed)
+            {
+                if (medicine.getid() == sellid)
+                {
+                    if (medicine.getcurrentquantity() >= Convert.ToInt32(soldq.Text))
+                    {
+                        medicine.setsoldquan(Convert.ToInt32(soldq.Text));
+                        medicine.setcurrentquantity(Convert.ToInt32(soldq.Text));
+                        medicine.setcurrentamount();
+                        MessageBox.Show("Medicine Sold.");
+                        break;
+                    }
+                    else
+                    {
+                        MessageBox.Show("This medicine cannot be sold!");
+                        break;
+                    }
+                }
+            }
+        }
+
+        private void Show_Click(object sender, EventArgs e)
+        {
+            foreach (Medicine medicine in listofmed)
+            { 
+                if (medicine.getid() == CheckID.Text)
+                {
+                    if (medicine.getsoldquantity()!= 0)
+                    { 
+                        CurrQ.Text = "Current Quantity: " + Convert.ToString(medicine.getcurrentquantity());
+                    }
+                    else
+                    {
+                        CurrQ.Text = "Current Quantity: " + Convert.ToString(medicine.getinquantity());
+                    }
+                    break;
+                }
+
+            }
+        }
+
+        private void ShowBalance_Click(object sender, EventArgs e)
+        {
+            int total =0;
+            foreach (Medicine medicine in listofmed)
+            {
+                 medicine.setcurrentamount();
+             total = total + (medicine.getcurrentamount());
+            }
+            //CurrBalance.Text =
+            MessageBox.Show("Current Balance: " + total.ToString());
+        }
+    }
+}
