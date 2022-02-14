050c209927f8f58aafe3fe8bf38b50ff515e21dc
Nazia-Karim-Khan-Oishee <95170855+Nazia-Karim-Khan-Oishee@users.noreply.github.com>
2022-02-14 22:47:17 +0600

    Add files via upload

---
+++ Lab8_SWE4202/SavingsAccount.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab8_SWE4202
+{
+    internal class SavingsAccount:Account
+    {
+        public int TransactionLimit = 5;
+        public int TransactionNumber;
+        public int WithdrawCharge=15;
+        public override string getID()
+        {
+            return this.AccNO = Convert.ToString(ID++) + "314";
+        }
+        public void setCurrentAccountBalance(int money)
+        {
+            this.MinAmount = money;
+            this.Balance = this.Balance + money;
+
+        }
+        public override void getBalance(int amount)
+        {
+            this.Balance = this.Balance + amount;
+        }
+        public SavingsAccount()
+        {
+            this.TransactionNumber = 0;
+        }
+        public void setinfo(string name, string contact)
+        {
+            this.Name = name;
+            this.Contact = contact;
+        }
+        public override void getNewBalance(int amount)
+        {
+            this.Balance = (this.Balance) - amount ;
+        }
+    }
+}
